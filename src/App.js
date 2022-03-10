06f6c067b0559e6073cc8597e8c8ba1123a8c059
Alexander Aboutanos <alexanderaboutanos@gmail.com>
2022-03-10 07:30:25 -0500

    0.2 - added basic components: auth pages, loading spinner, homepage, alert

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 /** @format */
 
-import logo from "./logo.svg";
 import "./App.css";
+import Button from "react-bootstrap/Button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Home of the future TeaTime Application!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="primary">Bootstrap btn</Button>
     </div>
   );
 }
---
+++ src/common/LoadingSpinner.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import React from "react";
+import "./LoadingSpinner.css";
+
+/** Loading message used by components that fetch API data. */
+
+function LoadingSpinner() {
+  return <div className="LoadingSpinner">Loading ...</div>;
+}
+
+export default LoadingSpinner;
